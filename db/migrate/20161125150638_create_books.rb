7d1d6c94b99877585f2eb471110cc9f3c4a9119c
Hummeltron <kay.stipcevic@gmail.com>
2016-11-27 01:22:25 +0100

    Backup loaded

--- db/migrate/20161125150638_create_books.rb
+++ db/migrate/20161125150638_create_books.rb
@@ -3,7 +3,7 @@ class CreateBooks < ActiveRecord::Migration
     create_table :books do |t|
       t.string :label
       t.float :price
-      t.string :isbn
+      t.text :isbn
       t.string :image
 
       t.timestamps null: false
